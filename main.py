289635475e3a6dc19db5cbbb27b34449b82d4b7d
Tanmay Khandelwal <tanmaykhandelwal2014@gmail.com>
2021-05-09 00:25:29 +0530

    Working with Headed mode without need to scan QR code.

--- main.py
+++ main.py
@@ -9,44 +9,54 @@ import time
 driver_path = r'C:\Users\tanma\PycharmProjects\chromedriver.exe'
 options = webdriver.ChromeOptions()
 # options.add_argument('headless')
-# options.add_argument('window-size=1920x1080')
+options.add_argument("--window-size=1440, 900")
 # options.add_argument("disable-gpu")
-options.add_argument(r'--user-data-dir=C:\\Users\\tanma\\AppData\\Local\\Google\\Chrome\\User Data\\wtsp')  # here is no <Default>!
+options.add_argument("user-data-dir=C:\\Users\\tanma\\AppData\\Local\\Google\\Chrome\\dataset")
 driver = webdriver.Chrome(executable_path=driver_path, options=options)  # selenium 4 prefers "options"
 # driver.maximize_window()
 wait = WebDriverWait(driver, 600)
 i = 0
+t = time.localtime()
 name = "Crypto Price tracker"
 while 1:
     try:
         i += 1
-        driver.get("https://coinswitch.co/coins/dogecoin/dogecoin-to-inr")
-        time.sleep(3)
+        while 1:
+            try:
+                driver.get("https://coinswitch.co/coins/dogecoin/dogecoin-to-inr")
+                break
+            except:
+                pass
         print('{}: coinswitch website accessed'.format(i))
-        g = driver.find_elements_by_class_name('assets__price')
-        a = 'Doge: ' + g[20].text
-        print(a)
-        driver.get("https://web.whatsapp.com/")
-        time.sleep(5)
-        search_box = WebDriverWait(driver, 500).until(EC.presence_of_element_located(
+        g = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@data-asset="Dogecoin"]'))).text
+        price = g[:4] + ':' + g[10:]
+        print(price)
+        while 1:
+            try:
+                driver.get("https://web.whatsapp.com/")
+                break
+            except:
+                pass
+        search_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
             (By.CLASS_NAME, '_2_1wd.copyable-text.selectable-text')))
         pyperclip.copy(name)
         search_box.send_keys(Keys.CONTROL + "v")
         group = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
             (By.XPATH, '//span[@title = "{}"]'.format(name))))
-        # driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
-        # time.sleep(10)
         group.click()
         print('Whatsapp group found')
-        msg_box = WebDriverWait(driver, 500).until(EC.presence_of_element_located(
+        msg_box = WebDriverWait(driver, 100).until(EC.presence_of_element_located(
             (By.XPATH, '//div[@contenteditable="true"][@data-tab="6"]')))
-        # msg_box = driver.find_element_by_xpath('//div[@contenteditable="true"][@data-tab="6"]')
-        # time.sleep(3)
         msg_box.clear()
-        msg_box.send_keys(a)
+        msg_box.send_keys(price)
         msg_box.send_keys(Keys.ENTER)
-        print('Price sent')
+        time.sleep(2)
+        current_time = time.strftime("%H:%M:%S", t)
+        print('Price sent at ', current_time)
+        driver.get('https://www.google.com/')
         time.sleep(60)
     except:
+        print("Going into error handling mode")
         continue
 driver.close()
